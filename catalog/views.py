adf247f40a82ca80046e0bd7dadf243c2ee658c0
Nataly Veldina <221fuchs@gmail.com>
2024-12-20 22:55:02 +0500

    add post-form

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def test_message(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return redirect('catalog/test_form.html')
+    return render(request, 'catalog/contact.html')
--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
