d9ab604a89e83599336873cabd85d1dc6e451623
Nataly Veldina <221fuchs@gmail.com>
2025-02-08 22:08:26 +0500

    fix smtp data, upd envsample

--- config/settings.py
+++ config/settings.py
@@ -113,10 +113,10 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "users:login"
 
-EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "veldina-natalia@yandex.ru"
-EMAIL_HOST_PASSWORD = "idiaeexlrharxfzu"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
