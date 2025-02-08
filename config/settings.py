b8991b2539e5199a8d4e457cb3ff7cd99fcaf846
Nataly Veldina <221fuchs@gmail.com>
2025-02-08 21:29:43 +0500

    add users app, reg and auth functions, upd navmenu and config

--- config/settings.py
+++ config/settings.py
@@ -107,4 +107,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_URL = "users:login"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "veldina-natalia@yandex.ru"
+EMAIL_HOST_PASSWORD = "idiaeexlrharxfzu"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
